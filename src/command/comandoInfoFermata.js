3691c920d735d279850e9c2ce7f2c28fae352064
Marco Faretra <marcofaretra@MBP-di-Marco.lan>
2016-02-19 19:59:07 +0100

    Add numero_fermata, numero_linea example

--- src/command/comandoInfoFermata.js
+++ src/command/comandoInfoFermata.js
@@ -3,5 +3,5 @@ var mongo = require('../model/model');
 
 module.exports = function comandoInfoFermata(msg, match) {
 	mongo({telegramId:msg.from.id.toString(), name:msg.chat.first_name+" "+msg.chat.last_name, command:[{name:"numero_fermata", date: new Date()}]}, msg.from.id);
- 	bot.sendMessage(msg.chat.id, 'Inserisci al posto di "numero_fermata", il numero della fermata in cui ti trovi.');
+ 	bot.sendMessage(msg.chat.id, 'Inserisci al posto di "numero_fermata", il numero della fermata in cui ti trovi, ad esempio /70951');
 }
