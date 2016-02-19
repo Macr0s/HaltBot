b1a7cf97c1b3ecdc5daecaab8c4d8502091f5b09
Marco Faretra <marcofaretra@MBP-di-Marco.lan>
2016-02-19 19:29:56 +0100

    Test 1

--- src/command/comandoInfoLinea.js
+++
@@ -1,7 +0,0 @@
-var bot = require('../modules/telegram-bot');
-var mongo = require('../model/model');
-
-module.exports = function comandoInfoLinea(msg, match) {
-	mongo({telegramId:msg.from.id.toString(), name:msg.chat.first_name+" "+msg.chat.last_name, command:[{name:"numero_linea", date: new Date()}]}, msg.from.id);
- 	bot.sendMessage(msg.chat.id, 'Inserisci al posto di "numero_linea", il numero della linea di tuo interesse.');
-}
--- src/modules/telegram-bot/index.js
+++
@@ -1,6 +0,0 @@
-var NodeTelegramBotAPI = require('node-telegram-bot-api');
-var fs = require('fs')
-var path = require('path');
-var filePath = path.join(__dirname, 'telegramToken.txt');
-var token = fs.readFileSync(filePath, 'utf8');
-module.exports = new NodeTelegramBotAPI(token, {polling: true});
